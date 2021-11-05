94f386138082cd2f2e52a7b83a24c46903801ef6
Alik <90277937+zheskod@users.noreply.github.com>
2021-11-06 02:12:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -24,22 +24,25 @@ while int(len_password) <= 0:
     continue
 
 number_password = input('Будут ли пароль содержать цифры? (ДА/НЕТ): ')
-if number_password == 'ДА':
+if number_password.lower() == 'да':
     chars += digits
 
 char_upper_password = input('Будет ли пароль содержать прописные буквы? (ДА/НЕТ): ')
-if char_upper_password == 'ДА':
+if char_upper_password.lower() == 'да':
     chars += lowercase_letters
 
 char_lower_password = input('Будет ли пароль содержать строчные буквы? (ДА/НЕТ): ')
-if char_lower_password == 'ДА':
+if char_lower_password.lower() == 'да':
     chars += uppercase_letters
 
 symbol_password = input('Будет ли пароль содержать символы? (ДА/НЕТ): ')
-if symbol_password == 'ДА':
+if symbol_password.lower() == 'да':
     chars += punctuation
 
 ambiguous_char = input('Исключить не однозначные символы? (il1Lo0O) (ДА/НЕТ): ')
+if ambiguous_char.lower() == 'да':
+    for c in 'il1Lo0O':
+        chars = chars.replace(c, '')
 
 print('А вот и ваши пароли:')
 for _ in range(quantity):
